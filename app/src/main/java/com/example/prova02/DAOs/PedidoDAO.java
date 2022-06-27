e0f023bbff4aaeadbc8353fa4d8e078de27bbac7
Pedro Ferreira <pedro.ferreirasilva777gmail.com>
2022-06-27 08:26:06 -0300

    atualizacao das queries da PedidoDAO

--- app/src/main/java/com/example/prova02/DAOs/PedidoDAO.java
+++ app/src/main/java/com/example/prova02/DAOs/PedidoDAO.java
@@ -4,9 +4,13 @@ import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.Query;
+import androidx.room.Transaction;
 import androidx.room.Update;
 
 import com.example.prova02.Pedido;
+import com.example.prova02.PedidoUsuario;
+
+import java.util.List;
 
 @Dao
 public interface PedidoDAO {
@@ -22,14 +26,22 @@ public interface PedidoDAO {
 
     //QUERIES
 
-//    @Query("SELECT * FROM pedidos")
-//    List<Pedido> getAll();
+    @Query("SELECT * FROM pedidos")
+    List<Pedido> getAll();
+
+    @Transaction
+    @Query("SELECT * FROM pedidos WHERE idUsuario = :pid")
+    public List<PedidoUsuario> getPedidosUsuario(int pid);
+
+//    @Transaction
+//    @Query("SELECT * FROM pedidos WHERE idProduto = :produtoId")
+//    pulbic List<P>
 //
 //    @Query("SELECT * FROM pedidos WHERE id IN (:fids)")
 //    List<Pedido> loadAllByIds(int[] fids);
 //
-//    @Query("SELECT * FROM pedidos WHERE id LIKE :idPedido LIMIT 1")
-//    Pedido findById(int idPedido);
+    @Query("SELECT * FROM pedidos WHERE idPedido LIKE :id LIMIT 1")
+    Pedido findById(int id);
 //
 //    @Query("SELECT * FROM pedidos WHERE endereco_entrega LIKE :enderecoEntrega")
 //    List<Pedido> loadAllByEndereco(String enderecoEntrega);
