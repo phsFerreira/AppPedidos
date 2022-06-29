0f9d63fe0517cbc93b622812209e2d993726a4c8
Pedro Ferreira <pedro.ferreirasilva777gmail.com>
2022-06-29 01:14:48 -0300

    Layout do fragmentExibirPedido pronto.

--- app/src/main/java/com/example/prova02/Fragments/FragmentFecharPedido.java
+++ app/src/main/java/com/example/prova02/Fragments/FragmentFecharPedido.java
@@ -13,6 +13,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -27,10 +29,13 @@ public class FragmentFecharPedido extends Fragment implements View.OnClickListen
 
     Button btFecharPedido;
     provaDatabase provaDB;
+    TextView tvPreco, tvQuantidade, tvValorTotal;
+    RadioGroup rgPagamento;
+    String metodoPagamento;
 
     CarrinhoAdapter adapter;
     RecyclerView rvProdutosCarrinho;
-    private ArrayList<Produto> produtos=new ArrayList<>();
+    ArrayList<Produto> produtos=new ArrayList<>();
 
     public FragmentFecharPedido() {
         // Required empty public constructor
@@ -56,6 +61,18 @@ public class FragmentFecharPedido extends Fragment implements View.OnClickListen
         //BANCO
         provaDB=provaDatabase.getInstance(getActivity().getApplicationContext());
 
+        tvQuantidade=view.findViewById(R.id.tvQuantidade);
+        tvPreco=view.findViewById(R.id.tvPrecoProdSelecionado);
+        tvValorTotal=view.findViewById(R.id.tvValorTotal);
+        rgPagamento=(RadioGroup) view.findViewById(R.id.radioGroup);
+        rgPagamento.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int id) {
+                RadioButton btSelecionado=(RadioButton) radioGroup.findViewById(id);
+                metodoPagamento=btSelecionado.getText().toString();
+            }
+        });
+
         //RECEBENDO LISTA PASSADA POR PARAMETRO PELA ACTIVITY
         if(getArguments()!=null){
             produtos= (ArrayList<Produto>) getArguments().getSerializable("lista");
@@ -73,21 +90,19 @@ public class FragmentFecharPedido extends Fragment implements View.OnClickListen
 
             @Override
             public void btAdicionarItemClick(int position, Produto produto) {
-                TextView txtQuantidade = view.findViewById(R.id.tvQuantidade);
-                int quantidade = Integer.parseInt(txtQuantidade.getText().toString());
+                int quantidade = Integer.parseInt(tvQuantidade.getText().toString());
                 quantidade++;
-                txtQuantidade.setText(String.valueOf(quantidade));
+                tvQuantidade.setText(String.valueOf(quantidade));
             }
 
             @Override
             public void btRemoverItemClick(int position, Produto produto) {
-                TextView txtQuantidade = view.findViewById(R.id.tvQuantidade);
-                int quantidade = Integer.parseInt(txtQuantidade.getText().toString());
+                int quantidade = Integer.parseInt(tvQuantidade.getText().toString());
                 if (quantidade<=0){
-                    txtQuantidade.setText("0");
+                    tvQuantidade.setText("0");
                 }else {
                     quantidade--;
-                    txtQuantidade.setText(String.valueOf(quantidade));
+                    tvQuantidade.setText(String.valueOf(quantidade));
                 }
             }
         });
@@ -103,16 +118,14 @@ public class FragmentFecharPedido extends Fragment implements View.OnClickListen
     public void onClick(View view) {
         switch (view.getId()){
             case R.id.btFecharPedido:
-                TextView txtPreco = view.findViewById(R.id.tvPrecoProdSelecionado);
-                TextView txtQuantidade = view.findViewById(R.id.tvQuantidade);
 
-                float preco = Float.parseFloat(txtPreco.getText().toString());
-                int quantidade = Integer.parseInt(txtQuantidade.getText().toString());
-
-                float valorTotal= preco * quantidade;
-
-                TextView txtValorTotal= view.findViewById(R.id.tvValorTotal);
-                txtValorTotal.setText(String.valueOf(valorTotal));
+//                float preco = Float.parseFloat(tvPreco.getText().toString());
+//                int quantidade = Integer.parseInt(tvQuantidade.getText().toString());
+//
+//                float valorTotal= preco * quantidade;
+//
+//                tvValorTotal.setText(String.valueOf(valorTotal));
+                Toast.makeText(getActivity(), "pagamento: "+metodoPagamento, Toast.LENGTH_SHORT).show();
                 break;
         }
     }
